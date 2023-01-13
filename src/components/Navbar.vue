736e36e1d9a351aa9643a45a92c54777e1fa9a98
thomas <thomasmartuaps@gmail.com>
2023-01-14 00:43:10 +0700

    adding poke list main content

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,11 +1,29 @@
+import { RouterLink, RouterView } from 'vue-router'
+
 <template>
-<nav class="navbar navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand">Navbar</a>
-    <form class="d-flex">
-      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
+  <header>
+    <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
+
+    <!-- <div class="wrapper"> -->
+    <!-- <HelloWorld msg="PokeVue" /> -->
+    <!-- </div> -->
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+      <div class="container-fluid">
+        <a class="navbar-brand">PokeVue</a>
+        <ul class="navbar-nav flex-row flex-wrap bd-navbar-nav pt-2 py-md-0">
+          <RouterLink to="/">Home</RouterLink>
+          <RouterLink to="/about">About</RouterLink>
+        </ul>
+        <form class="d-flex">
+          <input
+            class="form-control me-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
+  </header>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import PokeList from "../components/PokeList.vue";
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <PokeList />
   </main>
 </template>
